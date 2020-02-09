112eefcd78b1774caf1573d9335033e30e674e93
Kelven Galvão <kelvengalvao@gmail.com>
2020-02-08 21:35:52 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/irvine5k/ranking_challenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.irvine5k.ranking_challenge
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
